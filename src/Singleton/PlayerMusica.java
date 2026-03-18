571f4024617d66680d7046e6a95ac5a170b2d895
KauaLops <12kauazin@gmail.com>
2026-03-18 19:51:43 -0300

    Reformat PlayerMusica.java with consistent spacing

--- src/Singleton/PlayerMusica.java
+++ src/Singleton/PlayerMusica.java
@@ -8,7 +8,7 @@ public class PlayerMusica {
 
     public void diminuirVolume() {
         ControleVolume.getInstance().diminuir(10);
-    }
+}
 
     public void mostrarVolume() {
         System.out.println("Volume atual: " + ControleVolume.getInstance().getVolume());
