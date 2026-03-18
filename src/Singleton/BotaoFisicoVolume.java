2422f54e68683e18de5348d2ab6e03565d95ce1a
KauaLops <12kauazin@gmail.com>
2026-03-18 19:50:42 -0300

    Fix formatting and ensure newline at end of file

--- src/Singleton/BotaoFisicoVolume.java
+++ src/Singleton/BotaoFisicoVolume.java
@@ -6,7 +6,7 @@ public class BotaoFisicoVolume {
 
     public void pressionarMais() {
         ControleVolume.getInstance().aumentar(5);
-    }
+     }
 
     public void pressionarMenos() {
         ControleVolume.getInstance().diminuir(5);
